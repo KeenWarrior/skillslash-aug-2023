a1d427919551340842149b862d8920c1d6d8080f
keenwarrior <anujgargcse@gmail.com>
2023-11-05 11:04:47 +0530

    L 14

--- module_1/lecture_14/instagram-clone/src/pages/HomePage.js
+++ module_1/lecture_14/instagram-clone/src/pages/HomePage.js
@@ -1,5 +1,11 @@
-export default function HomePage(){
-    return (
-        <h1>Home page</h1>
-    );
+import Post from "../components/Post";
+import samplePost from "../data/samplePost";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Post post={samplePost} />
+    </>
+  );
 }
